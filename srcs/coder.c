1cb9070f8d76ccd00087491a0beb96d83647fd01
csng98 <csengekakula@gmail.com>
2026-08-20 15:09:37 +0200

    check burnout

--- srcs/coder.c
+++ srcs/coder.c
@@ -6,7 +6,44 @@
 /*   By: csekakul <csekakul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 09:05:56 by csekakul          #+#    #+#             */
-/*   Updated: 2026/08/14 09:05:57 by csekakul         ###   ########.fr       */
+/*   Updated: 2026/08/20 11:17:05 by csekakul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "codexion.h"
+
+static void	coder_compile(t_coder *coder)
+{
+	acquire_dongles(coder);
+	pthread_mutex_lock(&coder->data->state_mutex);
+	coder->last_compile_start = get_time_ms();
+	pthread_mutex_unlock(&coder->data->state_mutex);
+	safe_print(coder->data, coder->id, "is compiling");
+	precise_sleep(coder->data->time_to_compile, coder->data);
+	release_dongles(coder);
+}
+
+void	*coder_routine(void *arg)
+{
+	t_coder	*coder;
+
+	coder = (t_coder *)arg;
+	if (coder->id % 2 == 0)
+		usleep(1000);
+	while (!is_simulation_over(coder->data))
+	{
+		coder_compile(coder);
+		if (is_simulation_over(coder->data))
+			break ;
+		safe_print(coder->data, coder->id, "is debugging");
+		precise_sleep(coder->data->time_to_debug, coder->data);
+		if (is_simulation_over(coder->data))
+			break ;
+		safe_print(coder->data, coder->id, "is refactoring");
+		precise_sleep(coder->data->time_to_refactor, coder->data);
+		pthread_mutex_lock(&coder->data->state_mutex);
+		coder->compiles_count++;
+		pthread_mutex_unlock(&coder->data->state_mutex);
+	}
+	return (NULL);
+}
--- srcs/monitor.c
+++ srcs/monitor.c
@@ -6,7 +6,69 @@
 /*   By: csekakul <csekakul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/14 09:06:10 by csekakul          #+#    #+#             */
-/*   Updated: 2026/08/14 09:06:11 by csekakul         ###   ########.fr       */
+/*   Updated: 2026/08/20 10:25:17 by csekakul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "codexion.h"
+
+static int	check_burnout(t_data *data, int i, long long now)
+{
+	long long	diff;
+
+	diff = now - data->coders[i].last_compile_start;
+	if (diff > data->time_to_burnout)
+	{
+		data->simulation_end = 1;
+		pthread_mutex_lock(&data->log_mutex);
+		printf("%lld %d burned out\n",
+			now - data->start_time, data->coders[i].id);
+		pthread_mutex_unlock(&data->log_mutex);
+		pthread_mutex_unlock(&data->state_mutex);
+		return (1);
+	}
+	return (0);
+}
+
+static int	check_all_coders(t_data *data, int *all_finished, long long now)
+{
+	int	i;
+
+	i = 0;
+	while (i < data->num_coders)
+	{
+		if (check_burnout(data, i, now))
+			return (1);
+		if (data->compiles_required == -1
+			|| data->coders[i].compiles_count < data->compiles_required)
+			*all_finished = 0;
+		i++;
+	}
+	return (0);
+}
+
+void	*monitor_routine(void *arg)
+{
+	t_data		*data;
+	int			all_finished;
+	long long	now;
+
+	data = (t_data *)arg;
+	while (1)
+	{
+		pthread_mutex_lock(&data->state_mutex);
+		now = get_time_ms();
+		all_finished = 1;
+		if (check_all_coders(data, &all_finished, now))
+			return (NULL);
+		if (data->compiles_required != -1 && all_finished)
+		{
+			data->simulation_end = 1;
+			pthread_mutex_unlock(&data->state_mutex);
+			return (NULL);
+		}
+		pthread_mutex_unlock(&data->state_mutex);
+		usleep(1000);
+	}
+	return (NULL);
+}
